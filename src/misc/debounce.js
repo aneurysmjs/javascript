92f0a3db46bd12956ee8226a0f8d3d132ca13d64
aneurysm <aneurysmjs@outlook.com>
2021-05-14 01:40:44 +0300

    feat(misc): add debounce

---
+++ src/misc/debounce.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @param {(...*) => any)} fn 
+ * @param {number} ms 
+ * @returns {(...args: any[]) => void}
+ */
+export default function debounce(fn, ms) {
+  let timeoutId;
+
+  return function (...args) {
+    /**
+     * if an event happens before the setTimeout has expired. 
+     * we cancel the setTimeout and reset it, so effectively
+     * what this is gonna do, it's to keep reseting the timer
+     * as longs as events are ocurring before the timeout has
+     * had chance to expire
+     */
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      fn(args);
+    }, ms);
+  }
+}
---
+++ src/misc/debounce.test.js
@@ -0,0 +1,24 @@
+import { jest } from '@jest/globals';
+import debounce from './debounce';
+
+jest.useFakeTimers();
+
+describe('debounce', () => {
+    test('execute just once', () => {
+        const func = jest.fn();
+        const debouncedFunc = debounce(func, 500);
+    
+        // Execute for the first time
+        debouncedFunc();
+    
+        // Move on the timer
+        jest.advanceTimersByTime(250);
+        // try to execute a 2nd time
+        debouncedFunc();
+    
+        // Fast-forward time
+        jest.runAllTimers();
+    
+        expect(func).toBeCalledTimes(1);
+    });
+});
