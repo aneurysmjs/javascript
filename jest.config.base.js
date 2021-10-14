f5a7cd1f27220c33ccede09c530bebab8970c6b9
aneurysm <aneurysmjs@outlook.com>
2021-10-14 23:58:25 +0300

    refactor(jest): use native ES6 modules

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,10 +1,10 @@
-module.exports = {
+export default {
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['(__tests__/.*.mock).(js)$'],
-  moduleFileExtensions: ['js'],
+  coveragePathIgnorePatterns: ['(__tests__/.*.mock).(js|mjs)$'],
+  moduleFileExtensions: ['js', 'mjs'],
   roots: ['<rootDir>/src', '<rootDir>/__tests__'],
   testEnvironment: 'jest-environment-node',
-  testRegex: '(/__tests__/.*.(test|spec)).(jsx?|tsx?)$',
+  testRegex: '(/__tests__/.*.(test|spec)).(js|mjs)$',
   transform: {},
   verbose: true,
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
-module.exports = {
+export default {
   projects: ['<rootDir>/packages/*'],
 };
+
