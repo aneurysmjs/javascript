8dc217e2454c5143e3b4ffbf6893c142ec7cb5c3
aneurysm <aneurysmjs@outlook.com>
2023-03-09 23:18:24 +0100

    feat(datastructures): add linked list

---
+++ packages/dataStructures/__tests__/linkedList.test.mjs
@@ -0,0 +1,91 @@
+import { jest } from '@jest/globals';
+
+import {
+  createNode,
+  printNodes,
+  linkedListValues,
+  sumLinkedList,
+  findTarget,
+  getNodeValueAtIndex,
+  createLinkedList,
+} from '../src/linkedList.mjs';
+
+const user = {
+  name: 'Miro',
+  special: 'Flash grenade',
+};
+
+describe('linkedList', () => {
+  describe('createNode', () => {
+    it('creates a linkedList node', () => {
+      const a = createNode('A');
+      expect(a.value).toBe('A');
+    });
+  });
+
+  describe('printNodes', () => {
+    it('calls a callback per each node', () => {
+      const d = createNode('D');
+      const c = createNode('C', d);
+      const b = createNode('B', c);
+      const a = createNode('A', b);
+
+      // const logger = jest.fn((val) => console.log('val', val));
+      const logger = jest.fn();
+
+      printNodes(a, logger);
+
+      expect(logger).toHaveBeenCalledTimes(4);
+    });
+  });
+
+  describe('linkedListValues', () => {
+    it('gets all he linked list values in an array', () => {
+      const d = createNode('D');
+      const c = createNode('C', d);
+      const b = createNode('B', c);
+      const a = createNode('A', b);
+
+      expect(linkedListValues(a)).toStrictEqual(['A', 'B', 'C', 'D']);
+    });
+  });
+
+  describe('sumLinkedList', () => {
+    it('gets the sum of all of the values from a linkedlist', () => {
+      const d = createNode(8);
+      const c = createNode(12, d);
+      const b = createNode(5, c);
+      const a = createNode(15, b);
+
+      expect(sumLinkedList(a)).toBe(40);
+    });
+  });
+
+  describe('findTarget', () => {
+    it('returns true is a node is found', () => {
+      const d = createNode('D');
+      const c = createNode('C', d);
+      const b = createNode('B', c);
+      const a = createNode('A', b);
+
+      expect(findTarget(a, 'B')).toBe(true);
+      expect(findTarget(a, 'Z')).toBe(false);
+    });
+  });
+
+  describe('getNodeValueAtIndex', () => {
+    it('gets a node at giiven index', () => {
+      const d = createNode('D');
+      const c = createNode('C', d);
+      const b = createNode('B', c);
+      const a = createNode('A', b);
+
+      const result = createLinkedList(['a', 'b', 'c', 'd']);
+
+      console.log('result', result);
+
+      expect(getNodeValueAtIndex(a, 2)).toBe('C');
+      expect(getNodeValueAtIndex(a, 5)).toBe(null);
+    });
+  });
+});
---
+++ packages/dataStructures/src/linkedList.mjs
@@ -0,0 +1,183 @@
+export const createNode = (value, next = null) => ({
+  value,
+  next,
+});
+
+/**
+ * iterative version
+ */
+export const printNodesIter = (head, logger) => {
+  let current = head;
+
+  while (current !== null) {
+    logger(current.value);
+    current = current.next;
+  }
+};
+
+export const printNodes = (head, logger) => {
+  if (head === null) {
+    return;
+  }
+
+  logger(head.value);
+
+  printNodes(head.next, logger);
+};
+
+export const linkedListValues2 = (head) => {
+  let current = head;
+  const values = [];
+
+  while (current !== null) {
+    values.push(current.value);
+    current = current.next;
+  }
+
+  return values;
+};
+
+export const linkedListValues = (head) => {
+  const values = [];
+
+  const fillValues = (node) => {
+    if (node !== null) {
+      values.push(node.value);
+      fillValues(node.next);
+    }
+  };
+
+  fillValues(head);
+
+  return values;
+};
+
+export const sumLinkedListIter = (head) => {
+  let current = head;
+  let sum = 0;
+
+  while (current !== null) {
+    sum += current.value;
+    current = current.next;
+  }
+
+  return sum;
+};
+
+export const sumLinkedList = (node) => {
+  if (node === null) {
+    return 0;
+  }
+
+  return node.value + sumLinkedList(node.next);
+};
+
+export const findTargetIter = (node, target) => {
+  let current = node;
+
+  while (current !== null) {
+    if (current.value === target) {
+      return true;
+    }
+    current = node.next;
+  }
+
+  return false;
+};
+
+export const findTarget = (node, target) => {
+  if (node === null) {
+    return false;
+  }
+
+  if (node.value === target) {
+    return true;
+  }
+
+  return findTarget(node.next, target);
+};
+
+export const getNodeValueAtIndexIter = (node, index) => {
+  let current = node;
+
+  let count = 0;
+
+  while (current !== null) {
+    if (count === index) {
+      return current.value;
+    }
+
+    count += 1;
+
+    current = current.next;
+  }
+
+  return null;
+};
+
+export const getNodeValueAtIndex = (node, index) => {
+  if (node === null) {
+    return null;
+  }
+
+  /**
+   * when index reaches 0, this is the node's value we want
+   */
+  if (index === 0) {
+    return node.value;
+  }
+
+  /**
+   * on recursive calls you decrease it by one
+   */
+  return getNodeValueAtIndex(node.next, (index -= 1));
+};
+
+// const print = (el, ...rest) => {
+//   if (el == null) {
+//     return;
+//   }
+
+//   console.log('el', el);
+
+//   print(...rest);
+// };
+
+// print(...values);
+
+/**
+ *
+ * @param {string[]} values
+ */
+export const createLinkedListIter = (values) => {
+  const head = createNode(values[0]?.toUpperCase());
+  let current = head;
+
+  for (let i = 1; i < values.length; i += 1) {
+    const node = createNode(values[i]?.toUpperCase());
+    current.next = node;
+
+    current = node;
+  }
+
+  return head;
+};
+
+/**
+ *
+ * @param {string[]} values
+ */
+export const createLinkedList = (values) => {
+  const buildList = (index) => {
+    if (index === values.length) {
+      return;
+    }
+    const node = createNode(values[index]);
+
+    node.next = buildList((index += 1));
+
+    return node;
+  };
+
+  return buildList(0);
+};
