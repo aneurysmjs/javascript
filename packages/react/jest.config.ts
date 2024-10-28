6ab6668fa3c6732943233f9e65fe4bba0d83a1ef
aneurysmjs <aneurysmjs@outlook.com>
2024-10-28 18:01:11 +0100

    chore(jest): setup @testing-library/jest-dom

---
+++ packages/react/jest.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  displayName: 'react',
+  // A list of paths to modules that run some code to configure
+  // or set up the testing framework before each test.
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@[/](.+)': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(sc|c)ss$': '<rootDir>/config/jest/cssTransform.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/config/jest/jestSetup.ts'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'mjs', 'ts', 'tsx'],
+};
+
+export default config;
+
+// export default {
+//   displayName: 'react',
+//   // A list of paths to modules that run some code to configure
+//   // or set up the testing framework before each test.
+//   testMatch: [
+//     '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs,ts,tsx}',
+//     '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs,ts,tsx}',
+//   ],
+//   testEnvironment: 'jsdom',
+//   moduleNameMapper: {
+//     '^@[/](.+)': '<rootDir>/src/$1',
+//   },
+//   transform: {
+//     '^.+\\.tsx?$': 'ts-jest',
+//     '^.+\\.(sc|c)ss$': '<rootDir>/config/jest/cssTransform.js',
+//   },
+//   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$'],
+//   moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'mjs', 'ts', 'tsx'],
+// };
